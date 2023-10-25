e11b6ed708e9d3227a7455d190a54736c777090e
willsepulvedam <willmersepulveda23@gmail.com>
2023-10-24 20:15:05 -0500

    creacion de login y register y proteccion de la pagina con has_secure_password

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,9 @@
 class ProductsController < ApplicationController
+  skip_before_action :protect_pages, only: [:index, :show]
+
   def index
     @categories = Category.order(name: :asc).load_async
-    @products = Product.all.with_attached_photo
-    if params[:category_id]
-      @products = @products.where(category_id: params[:category_id])
-    end
-    if params[:min_price].present?
-      @products = @products.where("price >= ?", params[:min_price])
-    end
-    if params[:max_price].present?
-      @products = @products.where("price <= ?", params[:max_price])
-    end
-    if params[:query_text].present?
-      @products = @products.search_full_text(params[:query_text])
-    end
-
-    order_by =  Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
-
-    @products = @products.order(order_by).load_async
-
-    @pagy, @products = pagy_countless(@products, items: 12)
+    @pagy, @products = pagy_countless(FindProducts.new.call(product_params_index).load_async, items: 12)
   end
 
   def show
@@ -63,6 +47,10 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo, :category_id)
+  end
+
+  def product_params_index
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by)
   end
 
   def product
